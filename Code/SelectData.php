ede906fd8aab15ff870bf1d07768add1aea693b5
Karanjit Gahunia <karanjit.gahunia@gmail.com>
2017-10-16 18:31:01 +1300

    Added function to compare login credentials

--- Code/SelectData.php
+++ Code/SelectData.php
@@ -3,8 +3,14 @@
 
 function selectTeamInitials($dbConn)
 {
-    $query = "SELECT teamInitials from team";
+    $query = "SELECT teamInitials FROM Team";
     return $result = pg_query($dbConn, $query);
 }
 
+function selectPrivilegedUser($dbConn, $username, $password)
+{
+	$query = "SELECT privilege, fullname FROM PrivilegedUser WHERE username = '$username' AND password = '$password'";
+  $result = pg_query($dbConn, $query);
+  return $row = pg_fetch_row($result);
+}
 ?>
