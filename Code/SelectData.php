283aa531d624b7551a3bfb468974611d87d7fbf6
Karanjit Gahunia <karanjit.gahunia@gmail.com>
2017-10-17 17:20:45 +1300

    Added functions to retrieve top teams data

--- Code/SelectData.php
+++ Code/SelectData.php
@@ -1,4 +1,3 @@
-
 <?php
 
 function selectTeamInitials($dbConn)
@@ -11,6 +10,18 @@ function selectPrivilegedUser($dbConn, $username, $password)
 {
 	$query = "SELECT privilege, fullname FROM PrivilegedUser WHERE username = '$username' AND password = '$password'";
   $result = pg_query($dbConn, $query);
-  return $row = pg_fetch_row($result);
+  return pg_fetch_row($result);
 }
+
+function selectActiveCompetition($dbConn){
+  $query = "SELECT competitionid FROM Competition WHERE active = 'true'";
+  $result = pg_query($dbConn, $query);
+  return pg_fetch_row($result)[0];
+}
+
+function selectTopScores($dbConn, $activeCompetition){
+  $query = "SELECT teaminitials, currentscore FROM TeamRecord WHERE competitionid = '$activeCompetition' ORDER BY currentscore DESC LIMIT 5";
+  return pg_query($dbConn, $query);
+}
+
 ?>
